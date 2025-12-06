e85c6b01392f86c793fbb70aa793aacdc92dcb11
Micro-StudioPLC <jfsimon@startmail.com>
2025-12-06 17:08:12 +0100

    Ready Rel 1.0

--- base_v1/base_v1.ino
+++ base_v1/base_v1.ino
@@ -1,3 +1,31 @@
+/******************************************************************************
+ *  File:        base_v1.ino
+ *  Project:     Micro-StudioPLC - CLV-DIY-V1
+ *  Author:      Jean-François Simon, Chrysalide Engineering
+ *  Created:     6/12/2025
+ *  Description: Micro-StudioPLC CLV-DIY-V1 Source Code - Open-Source
+ *  Contact:     jfsimon@startmail.com
+ *
+ ******************************************************************************/
+ /*
+  * Computer Conncected Digital Multi-Meter ref CLV-DIY-V1
+ * Copyright (C) 2025  Jean-François Simon, Chrysalide Engineering
+ *
+ * This program is free software: you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License as published by the Free Software
+ * Foundation, either version 3 of the License, or (at your option) any later version.
+  *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along with this
+ * program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+#define VER "1.0"
+
 void wait();
 
 void setup() {
@@ -18,7 +46,7 @@ void loop() {
 }
 
 void wait() {
-    digitalWrite(LED_BUILTIN, 1);
+  digitalWrite(LED_BUILTIN, 1);
   delay(60);
   digitalWrite(LED_BUILTIN, 0);
   delay(60);
